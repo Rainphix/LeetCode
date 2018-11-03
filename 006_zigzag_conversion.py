1fb8a7513a53dddb083c378f8855431da8b3a572
Ray <rainphix.8991@gmail.com>
2018-11-03 23:56:52 +0800

    Update 006_zigzag_conversion.py

---
+++ 006_zigzag_conversion.py
@@ -0,0 +1,27 @@
+class Solution:
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+
+        if numRows <= 1 or numRows >= len(s):
+            return s
+        arr = [''] * numRows
+        line, step = 0, -1
+        for c in s:
+            arr[line] += c
+            print(arr)
+            if line % (numRows - 1) == 0:
+                step = - step
+            line += step
+        return ''.join(arr)
+
+
+if __name__ == "__main__":
+
+    s = Solution()
+    ans = s.convert("PAYPALISHIRING",3)
+    print(ans)
+    exit()
