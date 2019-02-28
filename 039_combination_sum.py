16ea8a21c2c51decde84240eac622d519676d99e
Ray <rainphix.8991@gmail.com>
2019-02-28 08:35:48 +0800

    Create 039_combination_sum.py

---
+++ 039_combination_sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        # 每次对target判断，target小于当前i的值时退出
+        candidates.sort()
+        Solution.anslist = []
+        self.recursion(candidates,target,0,[])
+        return Solution.anslist
+    def recursion(self,candidates,target,start,valuelist):
+        if target == 0:
+            return Solution.anslist.append(valuelist)
+        for i in range(start,len(candidates)):
+            if candidates[i] > target:
+                return
+            self.recursion(candidates,target-candidates[i],i,valuelist + [candidates[i]])
+        return True
+
+if __name__ == '__main__':
+
+    s = Solution()
+    ans = s.combinationSum([1,2,3],5)
+    print(ans)
