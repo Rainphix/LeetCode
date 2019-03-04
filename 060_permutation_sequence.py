bc25997f1034948cac6d8dc4bff8d3cc33ab4231
Ray <rainphix.8991@gmail.com>
2019-03-04 19:39:48 +0800

    Create 060_permutation_sequence.py

---
+++ 060_permutation_sequence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getPermutation(self, n, k): # n:元素个数 k:第k个排列
+        return n
+
+if __name__ == '__main__':
+
+    s = Solution()
+    ans = s.getPermutation(3,3)
+    print(ans)
