c22a8802902bc822e681f42bc530fe341b004822
Ray <rainphix.8991@gmail.com>
2019-03-23 21:02:00 +0800

    Create 056_merge_intervals.py

---
+++ 056_merge_intervals.py
@@ -0,0 +1,14 @@
+# Definition for an interval.
+# class Interval:
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution:
+    def merge(self, intervals: List[Interval]) -> List[Interval]:
+        return False
+
+if __name__ == '__main__':
+    s = Solution()
+    ans = s.merge([[1,2],[2,30]])
+    print(ans)
