e7de32e359a2f4c8864afcc671f6dd1240839733
Your Name <email@example.com>
2019-12-10 14:06:41 +0800

    Delete 085_maximal_rectangle.py

--- 085_maximal_rectangle.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        return False
-
-if __name__ == '__main__':
-
-    s = Solution()
-    ans = s.maximalRectangle(1)
-    print(ans)
