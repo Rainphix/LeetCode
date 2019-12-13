55036f967f10809de401b52c56a4a9d93ce20fd5
Your Name <email@example.com>
2019-12-13 15:20:41 +0800

    Update 056_merge_intervals.py
    100%

--- 056_merge_intervals.py
+++ 056_merge_intervals.py
@@ -1,14 +1,32 @@
-# Definition for an interval.
-# class Interval:
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
-
 class Solution:
-    def merge(self, intervals: List[Interval]) -> List[Interval]:
-        return False
+    def merge(self, intervals):
+
+        if not intervals:
+            return []
+        merged = []
+        l, r = 0, 0
+        cur = 0
+        intervals=sorted(intervals)
+
+        while(cur < len(intervals)-1):
+
+            l = intervals[cur][-1]
+            r = intervals[cur+1][0]
+
+            if l >= r:
+                intervals[cur+1][0] = intervals[cur][0]
+                if intervals[cur+1][-1] < intervals[cur][-1]:
+                    intervals[cur + 1][-1] = intervals[cur][-1]
+            else:
+                merged.append(intervals[cur])
+            cur += 1
+
+        merged.append(intervals[-1])
+        return merged
 
 if __name__ == '__main__':
     s = Solution()
-    ans = s.merge([[1,2],[2,30]])
+    # intervals = [[1,9],[2,11],[3,19],[4,15],[9,10]]
+    intervals = [[1,4],[0,4]]
+    ans = s.merge(intervals)
     print(ans)
